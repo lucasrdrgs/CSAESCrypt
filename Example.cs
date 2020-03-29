5c47151f73e3522de5d1f3322469c9747ea3094c
Lucas Rodrigues <33637247+lucasrdrgs@users.noreply.github.com>
2020-03-29 20:32:12 -0300

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -2,19 +2,16 @@ using System;
 using System.IO;
 using CsAesCrypt;
 
-namespace Example
-{
-	class ExampleProgram
-	{
-		static void Main(string[] args)
-		{
+namespace Example {
+	class ExampleProgram {
+		static void Main(string[] args) {
 			// This will create a CsAesCrypt instance with randomly generated key & IV
 			CsAesCrypt c = new CsAesCrypt();
 			
-			// If you want to use your own key & iv, do this:
-			// CsAesCrypt c = new CsAesCrypt(yourKey, yourIV);
-			// Please use a 32 char long key and a 16 char long IV.
-			
+			// If you want to use custom key and IV, pass them as arguments
+			// to the constructor: ... = new CsAesCrypt(yourKey, yourIV);
+			// PS: key must be 32 chars long and IV must be 16 chars long.
+
 			string path = Console.ReadLine();
 			if(!File.Exists(path)) {
 				throw new Exception("Invalid path!");
